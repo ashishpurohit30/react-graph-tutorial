ed49208a3158ef4c255c217ccdc55768da0c5044
Ashish Purohit <ashish.purohit@freshgravity.com>
2021-02-10 13:27:03 +0530

    React MS Graph Tutorial

---
+++ src/Config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    appId: '9f01d0f4-2c50-482f-ab1e-45cedb3cec7f',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+      'user.read',
+      'mailboxsettings.read',
+      'calendars.readwrite'
+    ]
+  };
